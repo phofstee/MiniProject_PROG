b47130b62017dd8cb35bb828a8b1c4bc5171ea38
Pim Hofstee <pim@hofstee.org>
2016-10-31 13:58:25 +0100

    Opzet Main file

---
+++ main.py
@@ -0,0 +1,21 @@
+import _sqlite3
+# @TODO: IMPORTS
+
+# Iedere module hier importeren
+# @AUTHOR: iedereen
+
+# @TODO: DB CONNECTIE
+# @AUTHOR Thomas
+
+# @TODO: REGISTREER
+# @AUTHOR: Pim
+
+# @TODO: STALLEN
+# @AUTHOR:
+
+# @TODO: OPHALEN
+# @NOTE: Hier krijg je
+# @AUTHOR:
+
+# @TODO: INFO OPVRAGEN
+# @AUTHOR:
