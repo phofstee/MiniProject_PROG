6e435a4f537bd7579b6245a6c5d3f9b82ba22857
Pim Hofstee <pim@hofstee.org>
2016-11-01 10:18:13 +0100

    Inlog menu gemaakt, stalling menu gemaakt, park aangepast

--- menu.py
+++ menu.py
@@ -2,28 +2,30 @@ __author__ = "Thomas Korevaar, Menno Noltes, Gijs van Ewijk, Larsse Vink and Pim
 __version__ = 1.0
 __status__ = 'Development'
 
-
-# Programme modules
 from register import register
-from login import login
+from park import park
 
-
-# Geeft menu opties weer
 def menu():
 
     print('Menu: ')
-    print('1 - Inloggen')
-    print('2 - Registreren')
+    print('1 - Fiets stallen')
+    print('3 - Fiets ophalen')
+    print('4 - Status fiets opvragen')
     print('')
 
     try:
         input_choice = int(input('Maak een keuze: '))
-        if input_choice > 0 and input_choice < 3:
+        if input_choice > 0 and input_choice < 5:
             if input_choice == 1:
-                login()
+                # todo: extra beveiliging > registratie?
+                park()
             if input_choice == 2:
                 # @todo: wachtwoord encrypten bij opslaan?
                 register()
+            if input_choice == 3:
+                print('Fiets ophalen')
+            if input_choice == 4:
+                print('Status fiets opvragen')
         else:
             print('Geen geldige invoer!')
     except:
@@ -32,6 +34,3 @@ def menu():
 
     # Na dat de functie is uitgevoerd, moet het menu opnieuw worden gestart.
     menu()
-
-# Roep menu aan bij opstarten script
-menu()
--- park.py
+++ park.py
@@ -7,33 +7,7 @@ def park():
 
     # settings for SQL connection
     sqlconn, sqlcursor = sqlconnection.InitializeSQL()
-    stallingData = sqlconnection.GetAllRows(sqlcursor)
 
-    users = []
-    accounts = []
-
-    for row in stallingData:
-        users.append(row[1])
-        accounts.append([row[0], row[1], row[2]])
-
-    print('Voordat u uw fiets kunt stallen, moet u eerst inloggen.')
-    print('')
-
-    # Enter username and check if username exists
-    name = str.lower(input('Gebruikersnaam: '))
-    while name not in users:
-        print('Gebruiker komt niet voor in database. Probeer het nogmaals.')
-        name = str.lower(input('Gebruikersnaam: '))
-
-    # Enter password and check if password does match
-    for account in accounts:
-        if account[1] == name:
-            account_password = account[2]
-
-    password = str(input('Wachtwoord:'))
-    while account_password != password:
-        print('Uw wachtwoord is onjuist. Probeer het nogmaals.')
-        password = str(input('Wachtwoord:'))
 
     # username and password are match, update database
     # create storage date
