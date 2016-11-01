95d6c5002d9cbcc0b0fca4c811cf5cc59587c7b0
Thomas Korevaar <thomaskorevaar.dev@gmail.com>
2016-11-01 14:47:42 +0100

    login.py bugfix

--- login.py
+++ login.py
@@ -19,26 +19,28 @@ def login():
 
     # Voer gebruikersnaam in en test of deze voorkomt
     name = str.lower(input('Gebruikersnaam: '))
-    while name not in users and user_login_attempts < 2:
+    while name not in users and user_login_attempts <= 1:
         user_login_attempts = user_login_attempts + 1
         print('Gebruiker komt niet voor in database. Probeer het nogmaals.')
         name = str.lower(input('Gebruikersnaam: '))
 
-    if user_login_attempts < 2:
+    if user_login_attempts <= 2 and name in users:
         # Voer wachtwoord in en kijk of deze klopt met het account
         for account in accounts:
             if account[1] == name:
                 account_uuid = account[0]
                 account_password = account[2]
 
-            password = str(input('Wachtwoord: '))
-        while account_password != password and password_login_attempts < 2 and user_login_attempts != 2:
+        password = str(input('Wachtwoord: '))
+        while account_password != password and password_login_attempts <= 1:
             password_login_attempts = password_login_attempts + 1
             print('Uw wachtwoord is onjuist. Probeer het nogmaals.')
             password = str(input('Wachtwoord: '))
 
-        if user_login_attempts < 2 or password_login_attempts < 2:
-            print('U heeft te vaak geprobeerd in te loggen, u wordt terug gestuurd naar het menu.')
-            time.sleep(2)
-
+    if name in users and account_password == password:
         return True, account_uuid
+    else:
+        print('U heeft te vaak geprobeerd in te loggen, u wordt terug gestuurd naar het menu.')
+        time.sleep(2)
+        return False, ""
+
